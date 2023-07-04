43301ebedba1fa04c182597ae4d6d52d99ecee70
s0undarya <s0undarya@192.168.137.80>
2023-07-04 18:49:50 +0530

    BUILd B01

---
+++ Hybrid/src/generic/script1.java
@@ -0,0 +1,27 @@
+package generic;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.io.FileHandler;
+
+public class script1 {
+
+	public static void main(String[] args) throws InterruptedException, IOException {
+		System.setProperty("webdriver.chrome.driver", "./softwares/chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://skpatro.github.io/demo/links/");
+		Thread.sleep(2000);
+		TakesScreenshot ts=(TakesScreenshot)driver;
+		File src = ts.getScreenshotAs(OutputType.FILE);
+		File dst = new File("D:\\jj\\popup.jpeg");
+		FileHandler.copy(src, dst);
+		
+
+	}
+
+}
---
+++ Hybrid/src/pom/Pom_Class1.java
@@ -0,0 +1,30 @@
+package pom;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class Pom_Class1 {
+@FindBy(id="email")
+private WebElement un;
+@FindBy(id="pass")
+private WebElement pwd;
+@FindBy(name="login")
+private WebElement log;
+public Pom_Class1(WebDriver driver) {
+	PageFactory.initElements(driver, this);
+}
+public void un_fild()
+{
+	un.sendKeys("abcdefgh");
+}
+public void pwd_field()
+{
+	pwd.sendKeys("123");
+	}
+public void login_click() 
+{
+	log.click();
+	}
+}
